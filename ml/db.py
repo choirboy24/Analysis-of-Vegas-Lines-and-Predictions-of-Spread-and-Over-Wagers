596f360d577b7d679b6104d57352ad1ea760405b
choirboy24 <124008318+choirboy24@users.noreply.github.com>
2025-08-07 00:23:21 -0600

    Delete ml/db.py

--- ml/db.py
+++
@@ -1,7 +0,0 @@
-# Not for version 1.0.0...will be for version 1.1.0
-
-from sqlalchemy import create_engine
-import psycopg2
-import pandas as pd
-
-alchemy_engine = create_engine('postgresql+psycopg2://')
